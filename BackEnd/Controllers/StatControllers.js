6de648ee3003bfae726e24059df0dd788d52874d
Oussama-Hassan-Bannour <oussama.bannour@utina.io>
2022-10-29 17:43:41 +0100

    tot

---
+++ BackEnd/Controllers/StatControllers.js
@@ -0,0 +1,63 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { DbOptions } = require("../KnexConfig/config");
+const knex = require("../KnexConfig/config");
+
+require("dotenv").config();
+
+const getnbSimulation = async (req, res) => {
+  console.log(`%${new Date().toISOString().substring(0, 10)}%`);
+  knex("simulations")
+    .where(
+      "created_at",
+      "like",
+      `%${new Date().toISOString().substring(0, 10)}%`
+    )
+    .count("id as nb")
+    .then((rows) => {
+      res.json(rows[0].nb).status(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getnbMeeting = async (req, res) => {
+  console.log(`%${new Date().toISOString().substring(0, 10)}%`);
+  knex("reservation")
+    .count("id as nb")
+
+    .where("date", "like", `%${new Date().toISOString().substring(0, 10)}%`)
+    .then((rows) => {
+      res.json(rows[0].nb).status(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getnbcontacts = async (req, res) => {
+  console.log(`%${new Date().toISOString().substring(0, 10)}%`);
+  knex("contacts")
+    .where(
+      "created_at",
+      "like",
+      `%${new Date().toISOString().substring(0, 10)}%`
+    )
+    .count("id as nb")
+    .then((rows) => {
+      res.json(rows[0].nb).status(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+module.exports = {
+  getnbSimulation,
+  getnbcontacts,
+  getnbMeeting,
+};
