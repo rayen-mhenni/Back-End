e188a5a41cbc43bfcf3ebf839132aeed71c01369
Oussama-Hassan-Bannour <oussama.bannour@utina.io>
2022-11-03 14:18:23 +0100

    stat

--- BackEnd/Controllers/StatControllers.js
+++ BackEnd/Controllers/StatControllers.js
@@ -11,7 +11,6 @@ const getnbSimulation = async (req, res) => {
       "created_at",
       "like",
       `%${new Date().toISOString().substring(0, 10)}%`
-      
     )
     .count("id as nb")
     .then((rows) => {
@@ -24,7 +23,6 @@ const getnbSimulation = async (req, res) => {
 };
 
 const getnbMeeting = async (req, res) => {
-
   knex("reservation")
     .count("id as nb")
 
@@ -39,7 +37,6 @@ const getnbMeeting = async (req, res) => {
 };
 
 const getnbcontacts = async (req, res) => {
-
   knex("contacts")
     .where(
       "created_at",
@@ -56,8 +53,148 @@ const getnbcontacts = async (req, res) => {
     });
 };
 
+const getnbSimulationNbr = async (req, res) => {
+  
+  let January = 0;
+  let February = 0;
+  let March = 0;
+  let April = 0;
+  let May = 0;
+  let June = 0;
+  let July = 0;
+  let August = 0;
+  let September = 0;
+
+  
+
+  await knex("simulations")
+    .where("created_at", "like", '%-01%')
+    .count("id as nb")
+    .then((rows) => {
+      January = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+    console.log("j",January )
+
+  await knex("simulations")
+    .where("created_at", "like", '%-02%')
+    .count("id as nb")
+    .then((rows) => {
+      February = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+  await knex("simulations")
+    .where("created_at", "like", '%-03%')
+    .count("id as nb")
+    .then((rows) => {
+      March = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+  await knex("simulations")
+    .where("created_at", "like", '%-04%')
+    .count("id as nb")
+    .then((rows) => {
+      April = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+  await knex("simulations")
+    .where("created_at", "like", '%-05%')
+    .count("id as nb")
+    .then((rows) => {
+      May = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+  await knex("simulations")
+    .where("created_at", "like", '%-06%')
+    .count("id as nb")
+    .then((rows) => {
+      June = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+  await knex("simulations")
+    .where("created_at", "like", '%-07%')
+    .count("id as nb")
+    .then((rows) => {
+      July = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+  await knex("simulations")
+    .where("created_at", "like", '%-08%')
+    .count("id as nb")
+    .then((rows) => {
+      August = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+  await knex("simulations")
+    .where("created_at", "like", '%-09%')
+    .count("id as nb")
+    .then((rows) => {
+      September = rows[0].nb;
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+
+
+ var months = [
+      
+        January,
+        February ,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+    
+    ];
+
+    const data = { months};
+
+ if (data) {
+      res.status(200).json(data);
+    } else res.status(500).json({ err });
+  
+
+};
+
 module.exports = {
   getnbSimulation,
   getnbcontacts,
   getnbMeeting,
+  getnbSimulationNbr
 };
--- BackEnd/Routers/StatRouters.js
+++ BackEnd/Routers/StatRouters.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getnbSimulation, getnbMeeting, getnbcontacts } = require("../Controllers/StatControllers");
+const { getnbSimulation, getnbMeeting, getnbcontacts, getnbSimulationNbr } = require("../Controllers/StatControllers");
 const router = express.Router();
 
 
 const verifyToken = require("../Middellware/AuthMiddelware");
 
 router.route("/simul").get(verifyToken, getnbSimulation);
+router.route("/simul/chart").get(verifyToken, getnbSimulationNbr);
 router.route("/meeting").get(verifyToken, getnbMeeting);
 router.route("/contact").get(verifyToken, getnbcontacts);
 
